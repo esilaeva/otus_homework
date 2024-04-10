d8157eb142bcde9310ce290c3a31ffe1864d2fe9
esilaeva <emsilaeva@gmail.com>
2024-04-10 21:11:05 +0300

    test: find earlier and latest courses

--- src/main/java/pages/CatalogPage.java
+++ src/main/java/pages/CatalogPage.java
@@ -1,7 +1,5 @@
 package pages;
 
-import static org.openqa.selenium.WebElement.*;
-
 import annotations.UrlPrefix;
 import com.google.inject.Inject;
 import java.io.IOException;
@@ -11,32 +9,56 @@ import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 @UrlPrefix("/catalog/courses")
 public class CatalogPage extends AnyPageAbs<CatalogPage> {
-  
-  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM, yyyy", new Locale("ru", "RU"));
-  Document page = Jsoup.connect(getUrl()).get();
-  
-  @Inject
-  public CoursePage coursePage;
-  
+
   @Inject
   public CatalogPage(WebDriver driver) throws IOException {
     super(driver);
   }
   
-  public void clickCourseByName(String courseName) {
+  public void clickCourseByName(String courseTitle) {
     driver.findElements(By.xpath("//section[2]//a[contains(@href, 'lessons')]//h6"))
         .stream()
-        .filter(e -> e.getText().contains(courseName))
+        .filter(e -> e.getText().contains(courseTitle))
         .findFirst().ifPresent(WebElement::click);
   }
   
+  public void test() {
+    List<Map<String, Object>> collected = findAllCourses();
+    
+    collected.stream().forEach(e -> System.out.format("Begin - %s - END\n\n", e));
+  }
+  
+  public List<Map<String, Object>> findAllCourses() {
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM, yyyy", new Locale("ru", "RU"));
+    Pattern datePattern = Pattern.compile("(\\d{1,2}\\s[а-яА-Я]+,\\s\\d{4})");
+    
+    return driver.findElements(By.xpath("//section[2]//a[contains(@href, 'lessons')]")).stream()
+        .map(e -> {
+          Map<String, Object> courseInfo = new HashMap<>();
+          courseInfo.put("title", e.findElement(By.cssSelector("h6")).getText());
+          
+          String dateString = e.findElement(By.cssSelector("h6+div>div:last-child")).getText();
+          LocalDate date = LocalDate.parse(Objects.requireNonNull(extractDate(dateString, datePattern, formatter)));
+          
+          courseInfo.put("date", date);
+          
+          return courseInfo;
+        })
+        .filter(map -> map.containsKey("date"))
+        .sorted(Comparator.comparing(map -> (LocalDate) map.get("date")))
+        .collect(Collectors.toList());
+  }
+  
+  private String extractDate(String text, Pattern datePattern, DateTimeFormatter formatter) {
+    Matcher matcher = datePattern.matcher(text);
+    LocalDate localDate = matcher.find() ? LocalDate.parse(matcher.group(1), formatter) : null;
+    
+    return localDate != null ? localDate.toString() : null;
+  }
 }
