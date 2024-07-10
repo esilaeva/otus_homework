8fd99a36b39e747165859eed324207fe32a794e7
esilaeva <emsilaeva@gmail.com>
2024-07-10 20:49:24 +0300

    add apiTests.groovy

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM maven:3.9.8
 
 USER root
 
-RUN mkdir -p /home/tests/ui-test
+RUN mkdir -p /home/tests/api-test
 
-WORKDIR /home/tests/ui-test
+WORKDIR /home/tests/api-test
 
 COPY . .
 
---
+++ jenkins/apiTests.groovy
@@ -0,0 +1,31 @@
+timeout(60) {
+    node("maven-cloud") {
+        def testContainerName = "apitests_$BUILD_NUMBER"
+
+//        try {
+            stage("Check Docker") {
+                script {
+                    def dockerVersion = sh(script: 'docker --version', returnStatus: true)
+                    if (dockerVersion != 0) {
+                        error "Docker is not installed or not found in PATH"
+                    }
+                }
+            }
+            stage("Run API tests") {
+                sh "docker run  --network=host --name ${testContainerName} -t localhost:5005/apitests:1.0"
+            }
+//            stage("Publish allure report") {
+//                allure([
+//                        disabled: true,
+//                        results: ["${pwd}/allure-results"]
+//                ])
+//            }
+//            stage("Telegram notification") {
+//                def allureReport = readFile(file: "${pwd}/allure-results/export/influxDbData.txt")
+//                Allure.addAttachment("Allure Report", "text/plain", allureReport)
+//            }
+//        } finally {
+//            sh "docker stop ${testContainerName}"
+//        }
+    }
+}
