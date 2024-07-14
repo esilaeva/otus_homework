5b80d8e2b0bebfaaa7a8b8c2f33bfc59427e7db6
esilaeva <emsilaeva@gmail.com>
2024-07-14 19:31:58 +0300

    update apiTests.groovy

--- jenkins/apiTests.groovy
+++ jenkins/apiTests.groovy
@@ -1,12 +1,17 @@
-timeout(60) {
-    node("maven-cloud") {
-        def testContainerName = "apitests_$BUILD_NUMBER"
-        def telegramToken = credentials('token')
-        def chatId = credentials('chatId')
-        def message = ""
-
-        try {
-            stage("Check Docker") {
+pipeline {
+    agent {
+        label 'maven-cloud'
+    }
+    environment {
+        TOKEN = credentials('token')
+        CHAT_ID = credentials('chatId')
+    }
+    options {
+        timeout(time: 60, unit: 'MINUTES')
+    }
+    stages {
+        stage("Check Docker") {
+            steps {
                 script {
                     def dockerVersion = sh(script: 'docker --version', returnStatus: true)
                     if (dockerVersion != 0) {
@@ -14,20 +19,29 @@ timeout(60) {
                     }
                 }
             }
-            stage("Run API tests") {
-                sh "docker run --network=host --name ${testContainerName} -v /home/jenkins/.m2:/root/.m2 -t localhost:5005/apitests:1.0"
-                message = "API tests completed successfully for build ${BUILD_NUMBER}"
+        }
+        stage('Run API tests') {
+            steps {
+                script {
+                    def testContainerName = "apitests_${env.BUILD_NUMBER}"
+                    // sh "docker run --network=host --name ${testContainerName} -v /home/jenkins/.m2:/root/.m2 -t localhost:5005/apitests:1.0"
+                    env.MESSAGE = "API tests completed successfully for build #${env.BUILD_NUMBER}"
+                }
             }
-        } catch (Exception e) {
-            message = "API tests failed for build ${BUILD_NUMBER}: ${e.message}"
-            throw e
-        } finally {
-            // Отправляем уведомление в Telegram
-            sh """
-        curl -s -X POST https://api.telegram.org/bot${telegramToken}/sendMessage \\
-            -d chat_id=${chatId} \\
-            -d text="${message}"
-    """
+            post {
+                failure {
+                    script {
+                        env.MESSAGE = "API tests failed for build #${env.BUILD_NUMBER}"
+                    }
+                }
+            }
+        }
+        stage('Telegram Notification') {
+            steps {
+                script {
+                    sh """curl -X POST -H 'Content-Type: application/json' -d '{"chat_id": "${env.CHAT_ID}", "text": "${env.MESSAGE}", "disable_notification": false}' https://api.telegram.org/bot${env.TOKEN}/sendMessage"""
+                }
+            }
         }
     }
 }
