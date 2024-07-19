4ad4a76ac1b077386b072464d7cc6b7f5d9f301e
esilaeva <emsilaeva@gmail.com>
2024-07-19 10:20:55 +0300

    update remote.properties

--- jenkins/ui-tests.groovy
+++ jenkins/ui-tests.groovy
@@ -1,40 +1,71 @@
-timeout(60) {
-    node("maven") {
-        def testContainerName = "ui_tests_$BUILD_NUMBER"
-
-        env.MY_PARAM = env.MYPARAM ?: ""
-
-        prepareConfig()
-        try {
-            wrap([$class: 'BuildUser']) {
-                currentBuild.description = "User: $BUILD_USER"
-            }
-            stage("Run UI tests ${jobDescription}") {
-                sh "docker run --rm --network=host --name ${testContainerName} -v $pwd/allure-results:/home/ubuntu/target/allure-results -t localhost:5005/ui-tests"
-            }
-            stage("Publish allure report") {
-                allure {[
-                        disabled: true,
-                        results: ["$pwd/allure-results"]
-                ]
+pipeline {
+    agent {
+        label 'maven-cloud'
+    }
+    environment {
+        TOKEN = credentials('token')
+        CHAT_ID = credentials('chatID')
+        //MAVEN_LOCAL_REPO = "/var/jenkins_home/.m2/repository"
+        BRANCH = 'main'
+    }
+    options {
+        timeout(time: 60, unit: 'MINUTES')
+    }
+    stages {
+        stage('Clone Repository') {
+            steps {
+                script {
+                    sh """
+                        echo "${MAVEN_CONFIG_HOME}"
+                        git clone -b ${env.BRANCH} https://github.com/esilaeva/otus_homework.git
+                    """
                 }
             }
-            stage("Telegram notification") {
-                def allureReport = readFile(text: "$pwd/allure-result/export/influxDbData.txt")
-                Allure.addAttachment("Allure Report", "text/plain", allureReport)
-
+        }
+        stage('Run Tests') {
+            steps {
+                script {
+                    sh """
+                        mkdir -p ${MAVEN_CONFIG_HOME}
+                        chown -R jenkins:jenkins ${MAVEN_CONFIG_HOME}
+                        chmod -R 775 ${MAVEN_CONFIG_HOME}
+                        mvn -Dmaven.repo.local=${MAVEN_CONFIG_HOME} -f otus_homework/pom.xml clean test
+                    """
+                }
             }
-        } finally {
-            sh "docker stop $testContainerName"
+            post {
+                always {
+                    allure includeProperties: false, jdk: '', results: [[path: 'otus_homework/allure-results']]
+                }
+            }
+        }
+        stage('Create Allure Report') {
+            agent none
+            steps {
+                script {
+                    env.MESSAGE = "API tests completed successfully for build #${env.BUILD_NUMBER}"
+                }
+            }
+            post {
+                failure {
+                    script {
+                        env.MESSAGE = "API tests failed for build #${env.BUILD_NUMBER}"
+                    }
+                }
+            }
+        }
+        stage('Telegram Notification') {
+            steps {
+                script {
+                    sh """
+                        curl -X POST -H 'Content-Type: application/json' -d '{
+                            "chat_id": "${env.CHAT_ID}",
+                            "text": "${env.MESSAGE}",
+                            "disable_notification": false
+                        }' https://api.telegram.org/bot${env.TOKEN}/sendMessage
+                    """
+                }
+            }
         }
     }
-}
-
-def prepareConfig() {
-    def yamlConfig = readYaml(text: "$YAML_CONFIG")
-    yamlConfig.each{k, v -> System.setProperty(v)}
-}
-
-def triggerJob(def jobName, def config) {
-    return build(job: "$jobName", parameters: ["YAML_CONFIG": config])
 }
