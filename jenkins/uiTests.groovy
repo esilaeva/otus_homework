f6de5caae552816259e2ac4d438a7ae1fecdbb26
esilaeva <emsilaeva@gmail.com>
2024-07-23 01:04:00 +0300

    update pipeline

--- jenkins/uiTests.groovy
+++ jenkins/uiTests.groovy
@@ -10,17 +10,19 @@ pipeline {
         TOKEN = credentials('token')
         CHAT_ID = credentials('chatID')
         JOB_NAME = "${env.JOB_NAME}"
-        BRANCH = "main"
+        DOCKER_HOME = "/home/ubuntu/ui-test"
+        BASE_URL = ""
+        BROWSER_NAME = ""
+        BROWSER_VERSION = ""
+        REMOTE_URL = ""
     }
     stages {
         stage('Prepare Environment') {
             steps {
                 script {
-                    // Создание/очистка директорий для отчетов
                     sh 'mkdir -p ${ALLURE_RESULTS} && rm -rf ${ALLURE_RESULTS}/*'
                     sh 'mkdir -p ${ALLURE_REPORT} && rm -rf ${ALLURE_REPORT}/*'
 
-                    // Получение параметров из YAML_CONFIG
                     def configText = params.YAML_CONFIG
                     def configMap = [:]
                     configText.split('\n').each { line ->
@@ -30,32 +32,36 @@ pipeline {
                         }
                     }
 
-                    BASE_URL = configMap['BASE_URL']
-                    BROWSER_NAME = configMap['BROWSER_NAME']
-                    BROWSER_VERSION = configMap['BROWSER_VERSION']
-                    REMOTE_URL = configMap['REMOTE_URL']
+                    env.BASE_URL = configMap['BASE_URL']
+                    env.BROWSER_NAME = configMap['BROWSER_NAME']
+                    env.BROWSER_VERSION = configMap['BROWSER_VERSION']
+                    env.REMOTE_URL = configMap['REMOTE_URL']
 
-                    echo "Configuration parsed successfully: BASE_URL=${BASE_URL}, BROWSER_NAME=${BROWSER_NAME}, BROWSER_VERSION=${BROWSER_VERSION}, REMOTE_URL=${REMOTE_URL}"
+                    echo "Configuration parsed successfully: BASE_URL=${env.BASE_URL}, BROWSER_NAME=${env.BROWSER_NAME}, BROWSER_VERSION=${env.BROWSER_VERSION}, REMOTE_URL=${env.REMOTE_URL}"
                 }
             }
         }
-        stage('Clone Repository') {
+        stage('Build and Test') {
             steps {
                 script {
-                    sh """
-                        echo "${MAVEN_CONFIG_HOME}"
-                        git clone -b ${env.BRANCH} https://github.com/esilaeva/otus_homework.git ${WORKSPACE}/otus_homework
-                    """
-                }
-            }
-        }
-        stage('Run Tests') {
-            steps {
-                script {
-                    sh """
-                        cd ${WORKSPACE}/otus_homework
-                        mvn clean test -Dtest=RunnerTest -DbaseUrl=${BASE_URL} -DbrowserName=${BROWSER_NAME} -DbrowserVersion=${BROWSER_VERSION} -DremoteUrl=${REMOTE_URL} -Dmaven.repo.local=${MAVEN_LOCAL_REPO}
-                    """
+                    // Запуск Docker контейнера и выполнение команд внутри него
+                    sh '''
+                        CONTAINER_ID=$(docker run --privileged -d \
+                            -v ${MAVEN_LOCAL_REPO}:${MAVEN_LOCAL_REPO} 192.168.88.193:5005/uitests:1.0 \
+                            /bin/bash -c "rm -rf ${DOCKER_HOME}/allure-results/* ${DOCKER_HOME}/allure-report/* && \
+                            mvn clean test -Denv=remote -Dmaven.repo.local=${MAVEN_LOCAL_REPO} && \
+                            allure generate ${DOCKER_HOME}/allure-results --clean -o ${DOCKER_HOME}/allure-report")
+                        
+                        # Просмотр логов выполнения тестов
+                        docker logs -f $CONTAINER_ID
+
+                        # Копирование содержимого результатов и отчетов из контейнера
+                        docker cp $CONTAINER_ID:/home/ubuntu/ui-test/allure-results/. ${ALLURE_RESULTS}/
+                        docker cp $CONTAINER_ID:/home/ubuntu/ui-test/allure-report/. ${ALLURE_REPORT}/
+
+                        docker stop $CONTAINER_ID || true
+                        docker rm $CONTAINER_ID || true
+                    '''
                 }
             }
         }
@@ -87,15 +93,13 @@ pipeline {
                 } catch (Exception e) {
                     env.REPORT_SUMMARY = "Failed to read Allure report: ${e.message}"
                 }
-                withCredentials([string(credentialsId: 'chatID', variable: 'CHAT_ID'), string(credentialsId: 'token', variable: 'TOKEN')]) {
-                    sh """
-                        curl -X POST -H 'Content-Type: application/json' -d '{
-                            "chat_id": "${CHAT_ID}",
-                            "text": "${env.MESSAGE}\\n${env.REPORT_SUMMARY}",
-                            "disable_notification": false
-                        }' https://api.telegram.org/bot${TOKEN}/sendMessage
-                    """
-                }
+                sh """
+                    curl -X POST -H 'Content-Type: application/json' -d '{
+                        "chat_id": "${env.CHAT_ID}",
+                        "text": "${env.MESSAGE}\\n${env.REPORT_SUMMARY}",
+                        "disable_notification": false
+                    }' https://api.telegram.org/bot${env.TOKEN}/sendMessage
+                """
             }
         }
     }
